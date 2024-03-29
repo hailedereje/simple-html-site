1cb59f3b3e5883e9b0ba7c4aea8c19ce1161434f
hailedereje <hailedereje88@gmail.com>
2024-03-29 13:33:55 +0300

    updating some files

--- php/public/index.php
+++ php/public/index.php
@@ -1,16 +1,20 @@
 <?php
    
     header("Access-Control-Allow-Origin: *");
-   $curl = curl_init();
-   $url = sprintf("%s?%s", "https://jsonplaceholder.typicode.com/comments", http_build_query(["postId=3"]));
-   curl_setopt($curl, CURLOPT_URL, $url);
-   curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
-
-   $result = curl_exec($curl);
-   echo $result;
-   if(!$result){die("Connection Failure");}
-   curl_close($curl);
+    $url = "https://jsonplaceholder.typicode.com/comments?postId=3";
     
-   return $result;
+    $curl = curl_init();
+    
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+
+    $result = curl_exec($curl);
+    
+    echo $result;
+    echo curl_error($curl);
+   
+    curl_close($curl);
+        
+    return $result;
 
 ?>
